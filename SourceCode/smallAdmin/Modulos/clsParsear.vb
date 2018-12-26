56481e36602a26139190dbbcbca410304976dc4a
walter <wveramdp@gmail.com>
2018-12-26 16:11:43 -0300

    se agrega modulo para leer archivo de pago

---
+++ SourceCode/smallAdmin/Modulos/clsParsear.vb
@@ -0,0 +1,23 @@
+﻿Imports libCommon.Comunes
+Public Class clsParsear
+
+  Private m_RawInput As New List(Of String)
+
+  Public Sub New(ByVal vList As List(Of String))
+    Try
+      m_RawInput = vList.ToList
+    Catch ex As Exception
+      Print_msg(ex.Message)
+    End Try
+  End Sub
+
+  Public Function GetPagos() As Result
+    Try
+
+      Return Result.OK
+    Catch ex As Exception
+      Print_msg(ex.Message)
+      Return Result.ErrorEx
+    End Try
+  End Function
+End Class
---
+++ SourceCode/smallAdmin/Modulos/modFile.vb
@@ -0,0 +1,33 @@
+﻿Imports libCommon.Comunes
+Module modFile
+
+  Public Function Load(ByVal pathFile As String, ByRef rLista As List(Of String)) As libCommon.Comunes.Result
+    Try
+      Dim Fila As Integer
+      Dim vResult As Result = Result.OK
+      Fila = FreeFile()
+      Try
+        FileOpen(Fila, pathFile, OpenMode.Input)
+        rLista = New List(Of String)
+        Do Until EOF(Fila)
+          rLista.Add(LineInput(Fila))
+        Loop
+
+        vResult = Result.OK
+      Catch ex As Exception
+        Print_msg(ex.Message)
+        vResult = Result.ErrorEx
+      Finally
+        FileClose(Fila)
+      End Try
+
+      Return vResult
+    Catch ex As Exception
+      Print_msg(ex.Message)
+      Return Result.ErrorEx
+    End Try
+  End Function
+
+
+
+End Module
