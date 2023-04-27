4b1956d9ae4e5c13a1da1c4ebb15b8cc936799e6
waltermdp <45598942+waltermdp@users.noreply.github.com>
2023-04-27 20:22:48 -0300

    pata

--- SourceCode/smallAdmin/Modulos/clsInfoPatagonia.vb
+++ SourceCode/smallAdmin/Modulos/clsInfoPatagonia.vb
@@ -1,15 +1,131 @@
-﻿Public Class clsInfoPatagonia
-  Private m_Secuencia As Integer
-  Private m_Producto As String
-  Private m_CodTran As Integer
+﻿Imports libCommon.Comunes
+Imports manDB
+
+Public Class clsInfoPatagonia
+
+
+  'body
+  Private m_TipoNovedad As String = "D"
+  Private m_Cuit_DNI As Decimal
+  Private m_CBU As Decimal
   Private m_ID_Cliente_Empresa As String
-  Private m_Documento As Integer
-  Private m_CBU As String
-  Private m_referencia As String
-  Private m_Importe As Decimal
   Private m_FechaVto As Date
-  Private m_FechaActual As Date
+  Private m_Producto As String
+
+  'Debitos
+  Private m_tipoMoneda As String = "P"
+  Private m_Importe As Decimal
+  Private m_ReferenciaDebito As String = "EDIT EL FARO"
+  Private m_NroCuitEmpresa As Decimal
+
+  Public Property TipoNovedad As String
+    Get
+      Return m_TipoNovedad
+    End Get
+    Set(value As String)
+      m_TipoNovedad = value
+    End Set
+  End Property
+
+  Public Property Cuit_DNI As Decimal
+    Get
+      Return m_Cuit_DNI
+    End Get
+    Set(value As Decimal)
+      m_Cuit_DNI = value
+    End Set
+  End Property
+
+  Public Property CBU As Decimal
+    Get
+      Return m_CBU
+    End Get
+    Set(value As Decimal)
+      m_CBU = value
+    End Set
+  End Property
+
+  Public Property ID_Cliente_Empresa As String
+    Get
+      Return m_ID_Cliente_Empresa
+    End Get
+    Set(value As String)
+      m_ID_Cliente_Empresa = value
+    End Set
+  End Property
+
+  Public Property FechaVto As Date
+    Get
+      Return m_FechaVto
+    End Get
+    Set(value As Date)
+      m_FechaVto = value
+    End Set
+  End Property
+
+  Public Property Producto As String
+    Get
+      Return m_Producto
+    End Get
+    Set(value As String)
+      m_Producto = value
+    End Set
+  End Property
 
 
+  Public Property TipoMoneda As String
+    Get
+      Return m_tipoMoneda
+    End Get
+    Set(value As String)
+      m_tipoMoneda = value
+    End Set
+  End Property
 
+  Public Property Importe As Decimal
+    Get
+      Return m_Importe
+    End Get
+    Set(value As Decimal)
+      m_Importe = value
+    End Set
+  End Property
+
+  Public Property ReferenciaDebito As String
+    Get
+      Return m_ReferenciaDebito
+    End Get
+    Set(value As String)
+      m_ReferenciaDebito = value
+    End Set
+  End Property
+
+  Public Property NroCuitEmpresa As Decimal
+    Get
+      Return m_NroCuitEmpresa
+    End Get
+    Set(value As Decimal)
+      m_NroCuitEmpresa = value
+    End Set
+  End Property
+
+  Public Sub New()
+    Try
+
+      TipoNovedad = "D"
+      Cuit_DNI = 0
+      CBU = 0
+      ID_Cliente_Empresa = " "
+      FechaVto = g_Today
+      Producto = "CUOTA"
+
+      TipoMoneda = "P"
+      Importe = 0
+      ReferenciaDebito = "EDIT EL FARO"
+      NroCuitEmpresa = 0
+
+    Catch ex As Exception
+      Print_msg(ex.Message)
+    End Try
+  End Sub
 End Class
--- SourceCode/smallAdmin/Modulos/clsPatagonia.vb
+++ SourceCode/smallAdmin/Modulos/clsPatagonia.vb
@@ -1,30 +1,82 @@
 ﻿Imports libCommon.Comunes
 Imports manDB
 Public Class clsPatagonia
+  'header
+  Private m_NroCuitEmpresa As Decimal
+  Private m_Producto As String
+  Private m_OrigenComercial As Decimal
+  Private m_FechaPresentacion As Date
+  Private m_Reserva As String
+  Private m_RazonSocial As String
+  'body
+
+  'tail
+  'T
+  Private m_CantidadRegistros As Decimal
+  Private m_ImporteTotal As Decimal
 
 
-
-  Private Function GenerateHeader() As Result
+  Private Function GenerateHeader(ByRef rLinea As String) As Result
     Try
       Dim sResult As String = String.Empty
 
-      sResult += "1"
-      sResult += m_Convenio.ToString("D5")
-      sResult += String.Format("{0,10}", " ")
-      sResult += New String("0"c, 5) ' String.Format("D5", "0")
-      sResult += vFechaGen.ToString("yyyyMMdd")
-      sResult += (vImporteTotal * 100).ToString("D18")
-      sResult += MONEDA.ToString("D3")
-      sResult += TIPOMOV.ToString("D2")
-      sResult += vSecuencial.ToString("D3")
-      sResult += New String("0"c, 95) 'String.Format("D95", "0")
-      sResult += String.Format("{0,69}", " ")
-      sResult += "0"
-      rHeader = sResult
+      sResult += "H"
+      sResult += m_NroCuitEmpresa.ToString("00000000000")
+      sResult += String.Format("{0,-10}", m_Producto)       'longitud=10
+      sResult += m_OrigenComercial.ToString("000")
+      sResult += m_FechaPresentacion.ToString("ddMMyyyy")
+      sResult += String.Format("{0,-12}", m_Reserva)
+      sResult += String.Format("{0,-35}", m_RazonSocial)
+      sResult += String.Format("{0,120}", " ")
+
+      rLinea = sResult
       Return Result.OK
     Catch ex As Exception
       Print_msg(ex.Message)
       Return Result.ErrorEx
     End Try
   End Function
+
+  Private Function GenerateRegistros(ByVal vRegistro As clsInfoPatagonia, ByRef rlinea As String) As Result
+    Try
+      Dim sResult As String = String.Empty
+
+      sResult += vRegistro.ToString()
+      sResult += vRegistro.vRegistro.vNroCasos.ToString("0000000")                            '7
+      'sResult += CDec(vImporteTotal * 100).ToString("000000000000000")    '15
+      'sResult += String.Format("{0,177}", " ")
+
+      rlinea = sResult
+      Return Result.OK
+    Catch ex As Exception
+      Print_msg(ex.Message)
+      Return Result.ErrorEx
+    End Try
+  End Function
+
+  Private Function GenerateTail(ByVal vNroCasos As Decimal, ByVal vImporteTotal As Decimal, ByRef rlinea As String) As Result
+    Try
+      Dim sResult As String = String.Empty
+
+      sResult += "T"
+      sResult += vNroCasos.ToString("0000000")                            '7
+      sResult += CDec(vImporteTotal * 100).ToString("000000000000000")    '15
+      sResult += String.Format("{0,177}", " ")
+
+      rlinea = sResult
+      Return Result.OK
+    Catch ex As Exception
+      Print_msg(ex.Message)
+      Return Result.ErrorEx
+    End Try
+  End Function
+
+  Public Sub New()
+    Try
+      m_Producto = "CUOTA"
+    Catch ex As Exception
+      Print_msg(ex.Message)
+    End Try
+  End Sub
+
 End Class
