6c0a028bd57f76f9fd0574731fd15e05051df76f
waltermdp <45598942+waltermdp@users.noreply.github.com>
2023-05-31 06:41:31 -0300

    back

--- SourceCode/smallAdmin/My Project/Resources.Designer.vb
+++ SourceCode/smallAdmin/My Project/Resources.Designer.vb
@@ -69,5 +69,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property FondoGral_960x540() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("FondoGral_960x540", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
